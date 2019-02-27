774ec631d3932b512c6a22c93e56a29e13ff4a02
Subhas <subhas@3web.com>
2019-02-27 14:34:01 -0500

    API and testing setup

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon'); // eslint-disable-line
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session')({
+  secret: 'any random string can go here',
+  resave: false,
+  saveUninitialized: false,
+});
+const User = require('./models/user');
 const indexRouter = require('./routes/index');
-//const usersRouter = require('./routes/users');
-const api =require('./routes/api/index');
+const api = require('./routes/api/index');
+const users = require('./routes/api/users');
 
 const app = express();
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/musiclist', { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// un comment after placing your favicon in public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', api);
+app.use('/api/users', users);
 
+// Configure Passport
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+app.use((req, res, next) => {  // eslint-disable-line
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { // eslint-disable-line
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
